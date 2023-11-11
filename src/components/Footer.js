39e66b33eeb1cacb8a8f5ec72c902f0e52f7e89c
thabitor <thabithamror@gmail.com>
2023-11-11 20:05:17 +0100

    fixed partner section in homepage

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ function Footer (props) {
                     <div className="col col-lg-3 col-md-6 col-sm-12 col-12">
                         <div className="widget about-widget">
                             <div className="logo widget-title">
-                                <img src='images/logo/logo-seso-sm.png' alt=""/>
+                                <img src='images/logo/logo-seso-xlg.png' alt=""/>
                             </div>
                             {/* <p>Build and Earn with your online store with lots of cool and exclusive wpo-features </p> */}
                             <ul>
